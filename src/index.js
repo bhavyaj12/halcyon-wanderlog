f55e32400d8fb29dc361f91f953f561995ea7bf0
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-05-28 19:15:53 +0530

    chore: added bootstrap and router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
