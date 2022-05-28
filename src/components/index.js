5012adaed3f253e405814636f9f6f8436b20d209
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-05-28 19:17:09 +0530

    feat: suggestions list component

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import NavigationTop from "./NavigationTop";
+import Footer from "./Footer";
+import NavSide from "./NavSide";
+import AddPost from "./AddPost";
+import PostCard from "./PostCard";
+import SuggestionList from "./SuggestionList";
+
+export { NavigationTop, Footer, NavSide, AddPost, PostCard, SuggestionList };
