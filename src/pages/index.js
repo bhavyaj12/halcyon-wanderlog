21748242ff8cdc8abbe099d75f700159b85ce361
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-05-30 23:45:02 +0530

    feat: added 404 page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,6 @@ import HomePage from "./HomePage";
 import UserHome from "./UserHome";
 import LoginPage from "./LoginPage";
 import SignupPage from "./SignupPage";
+import InvalidPage from "./InvalidPage";
 
-export { HomePage, UserHome, LoginPage, SignupPage };
+export { HomePage, UserHome, LoginPage, SignupPage, InvalidPage };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Mockman from "mockman-js";
-import { HomePage, UserHome, LoginPage, SignupPage } from "pages";
+import { HomePage, UserHome, LoginPage, SignupPage, InvalidPage } from "pages";
 import PrivateRoutes from "./PrivateRoutes";
 
 const SiteRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      {/* <Route path="*" element={<InvalidPage />} /> */}
+      <Route path="*" element={<InvalidPage />} />
       <Route path="/mockman" element={<Mockman />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
