ccd0ff6b0d9c7caaf99ea14b3f092224ee9f2805
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-06-01 21:45:27 +0530

    feat: added scroll to and theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { NavigationTop, Footer, Toast, PostModal } from "components";
 import { Routes } from "routes";
+import { useTheme } from "theme-context";
 import "./App.css";
 
 const App = () => {
+  const { theme } = useTheme();
+  console.log(theme);
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
   return (
-    <div className="App">
+    <div className={`App ${theme}`}>
         <NavigationTop />
         <Toast />
         <Routes />
