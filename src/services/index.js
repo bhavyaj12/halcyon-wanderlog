0f43496d1f8cfa9043d3b722d8184b4244e477c6
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-06-01 18:35:11 +0530

    feat: added api services for edit, like, dislike

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,21 @@
 import { loginService } from "./loginService";
 import { signupService } from "./signupService";
-import { fetchPostsService, addPostService, deletePostService } from "./postsServices";
+import {
+  fetchPostsService,
+  addPostService,
+  deletePostService,
+  editPostService,
+  likePostService,
+  dislikePostService,
+} from "./postsServices";
 
-export { loginService, signupService, fetchPostsService, addPostService, deletePostService };
+export {
+  loginService,
+  signupService,
+  fetchPostsService,
+  addPostService,
+  deletePostService,
+  editPostService,
+  likePostService,
+  dislikePostService,
+};
--- src/services/postsServices.js
+++ src/services/postsServices.js
@@ -12,10 +12,47 @@ const addPostService = (token, postData) => {
   );
 };
 
+const editPostService = (token, postData, postID) => {
+  return axios.post(
+    `/api/posts/edit/${postID}`,
+    { postData },
+    {
+      headers: { authorization: token },
+    }
+  );
+};
+
 const deletePostService = (token, postID) => {
   return axios.delete(`/api/posts/${postID}`, {
     headers: { authorization: token },
   });
 };
 
-export { fetchPostsService, addPostService, deletePostService };
+const likePostService = (token, postID) => {
+  return axios.post(
+    `/api/posts/like/${postID}`,
+    {},
+    {
+      headers: { authorization: token },
+    }
+  );
+};
+
+const dislikePostService = (token, postID) => {
+  return axios.post(
+    `/api/posts/dislike/${postID}`,
+    {},
+    {
+      headers: { authorization: token },
+    }
+  );
+};
+
+export {
+  fetchPostsService,
+  addPostService,
+  deletePostService,
+  editPostService,
+  likePostService,
+  dislikePostService,
+};
