3e020bd72d2ae6e45db9a3ca69f0aeb8aa7b6a5d
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-06-03 21:00:13 +0530

    chore: index files

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,5 +5,6 @@ import SignupPage from "./SignupPage";
 import InvalidPage from "./InvalidPage";
 import BookmarksPage from "./BookmarksPage";
 import SinglePost from "./SinglePost";
+import ProfilePage from "./ProfilePage";
 
-export { HomePage, UserHome, LoginPage, SignupPage, InvalidPage, BookmarksPage, SinglePost };
+export { HomePage, UserHome, LoginPage, SignupPage, InvalidPage, BookmarksPage, SinglePost, ProfilePage };
