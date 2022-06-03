92ab726bc9f288acf6d98cd964d1c94ccf11ab7f
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-06-03 20:57:18 +0530

    chore: updated store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authReducer, modalForPostReducer, postsReducer } from "redux-reducers";
+import { authReducer, modalForPostReducer, postsReducer, profileEditModalReducer, userProfileReducer } from "redux-reducers";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     posts: postsReducer,
     modalForPost: modalForPostReducer,
+    profileEditModal: profileEditModalReducer,
+    userProfile: userProfileReducer,
   },
 });
 
